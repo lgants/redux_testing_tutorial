60201cf9d77c640418637eb06f03070d4efb2eb7
lgants <lgants@gmail.com>
2017-01-13 23:01:24 -0600

    initial commit

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,7 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+// use 'describe' to group together similiar tests
 describe('App' , () => {
   let component;
 
@@ -8,7 +9,9 @@ describe('App' , () => {
     component = renderComponent(App);
   });
 
+  // use 'it' to test a single attribute of a target
   it('renders something', () => {
+    // use 'expect' to make an 'assertion' about a target
     expect(component).to.exist;
   });
 });
